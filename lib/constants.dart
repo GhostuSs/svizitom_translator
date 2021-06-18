f32fb37013b717499ef4542cc5ddf6c54b6d38e7
Maxim Movchan <maksmovchan@icloud.com>
2021-06-18 12:01:12 +0300

    '#'

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+//Russian
+String titleRu = 'Заголовок';
+String subtitleRu = 'Подзаголовок';
+var languagesRu = ['Русский','Английский','Украинский'];
+
+//English
+String titleEn = 'Title';
+String subtitleEn = 'Subtitle';
+var languagesEn = ['Russian','English','Ukrainian'];
+
+//Ukrainian
+String titleUa = 'Заголовок';
+String subtitleUa = 'Пiдзаголок';
+var languagesUa = ['Російський','Англійський','Украiньска'];
--- lib/translations.dart
+++ lib/translations.dart
@@ -1,29 +1,30 @@
 import 'package:get/get_navigation/src/root/internacionalization.dart';
 import 'package:get/get.dart';
+import 'constants.dart';
 
 class Translator extends Translations{
   @override
   Map<String, Map<String, String>> get keys => {
-    'en_US' : {
-      'title' : 'Title',
-      'subtitle': 'Subtitle',
-      'Russian':'Russian',
-      'English':'English',
-      'Ukrainian':'Ukrainian'
+    'ru_Ru' : {
+      'title' : titleRu,
+      'subtitle': subtitleRu,
+      'Russian':languagesRu[0],
+      'English':languagesRu[1],
+      'Ukrainian':languagesRu[2]
     },
-    'ru_RU' : {
-      'title' : 'Заголовок',
-      'subtitle': 'Подзаголовок',
-      'Russian':'Руский',
-      'English':'Английский',
-      'Ukrainian':'Украинский'
+    'en_US' : {
+      'title' : titleEn,
+      'subtitle': subtitleEn,
+      'Russian':languagesEn[0],
+      'English':languagesEn[1],
+      'Ukrainian':languagesEn[2]
     },
     'uk_UA' : {
       'title' : 'Заголовок',
       'subtitle': 'Підзаголовок',
-      'Russian':'Російський',
-      'English':'Англійський',
-      'Ukrainian':'Украiньска'
+      'Russian':languagesUa[0],
+      'English':languagesUa[1],
+      'Ukrainian':languagesUa[2]
     }
   };
 
